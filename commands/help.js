cc388817a5d826bda43735d5741a5fdcd25a92e1
b1theaven <mohammadrizky881@gmail.com>
2024-08-05 10:46:00 +0700

    updated command list

--- commands/help.js
+++ commands/help.js
@@ -7,18 +7,18 @@ module.exports = {
     aliases: ['h'],
     run: async (client, msg, args) => {
         const embed = new MessageEmbed()
-            .setColor('PURPLE')
+            .setColor('BLUE')
             .setAuthor(`${lang(msg.guild, 'COMMANDS_LIST')} - ${client.commands.size} ${lang(msg.guild, 'COMMANDS')}`)
             .setThumbnail(msg.guild.iconURL({ dynamic: true }))
             .addFields(
                 { name: '<:crystal_ball:885185036419219496> Boosters', value: stripIndents`
 > \`role create\`, \`role edit\`, \`role give\`` },
                 { name: '<:hammer:885195095840804885> Utility ' + lang(msg.guild, 'COMMANDS'), value: stripIndents`
-> \`avatar\`, \`ping\`, \`youtubedownload\`, \`emojilist\`, \`movie\`, \`say\`, \`remindme\`, \`cancelremind\`, \`listreminder\`, \`snipe\`, \`google\`, \`ytstats\`, \`afk\`` },
+> \`avatar\`, \`ping\`, \`youtubedownload\`, \`emojilist\`, \`movie\`, \`say\`, \`remindme\`, \`cancelremind\`, \`listreminder\`, \`snipe\`, \`google\`, \`ytstats\`, \`afk\`, \`nasa\`, \`cuddle\`, \`hug\`, \`kiss\`, \`pat\`, \`slap\`, \`youtube\`` },
                 { name: '<:shield:885185036419219496> Moderation', value: stripIndents`
 > \`setprefix\`, \`setlanguage\`, \`clear\`, \`addemoji\`, \`backup\`, \`kick\`, \`ban\`, \`giveaway\`, \`nuke\`, \`unban\`, \`reminder\`, \`removeremind\`` },
                 { name: "<:chart:885185036419219496> Slash "+ lang(msg.guild, "COMMANDS"), value: stripIndents`
-> \`avatar\`, \`serveravatar\`, \`automod-flaggedword\`, \`automod-keyword\`, \`automod-mentionspam\`, \`ban\`, \`kick\`, \`setlanguage\`, \`setprefix\`, \`reloadcommands\`, \`youtubedownload\`` },
+> \`avatar\`, \`imagine\`, \`serveravatar\`, \`automod-flaggedword\`, \`automod-keyword\`, \`automod-mentionspam\`, \`ban\`, \`kick\`, \`setlanguage\`, \`setprefix\`, \`leaderboard\`, \`reloadcommands\`, \`lyrics\`, \`youtubedownload\`, \`reactionrole-add\`, \`reactionrole-remove\`` },
                 { name: '<:information_source:885192830283313232> Information ' + lang(msg.guild, 'COMMANDS'), value: stripIndents`
 > \`steam\`, \`playstore\`, \`topgginfo\`, \`device\`, \`worldclock\`, \`permissions\`, \`whois\`, \`stats\`, \`leaderboard\`` },
             )
