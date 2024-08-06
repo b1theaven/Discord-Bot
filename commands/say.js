c50a04a564e25358d210656eb20c6111372966ac
b1theaven <mohammadrizky881@gmail.com>
2024-08-06 11:39:53 +0700

    update the code

--- commands/say.js
+++ commands/say.js
@@ -1,22 +1,31 @@
 module.exports = {
-    name: "Say",
-    run: async (client, msg, args) => {
-      let say = args.join(" ");
-  
-      if (!say) {
-        await msg.channel.send(await client.translate("Example: <prefix>.say test", msg));
-        return msg.delete(); // Menghapus pesan pengguna jika tidak ada teks
-      }
-  
-      if (say.length > 250) {
-        await msg.channel.send(await client.translate("The maximum character was 250", msg));
-        return msg.delete(); // Menghapus pesan pengguna jika teks melebihi 250 karakter
-      }
-  
-      say = say.replace("@everyone", "@everyone").replace("@here", "@here");
-  
-      await msg.channel.send(say);
-      msg.delete(); // Menghapus pesan pengguna setelah bot mengirimkan pesan
+  name: "say",
+  run: async (client, msg, args) => {
+    let say = args.join(" ");
+    const imagesSend = msg.attachments.first()
+    console.log(imagesSend)
+    
+    if(imagesSend) {
+       await msg.delete()
+       return msg.channel.send({ content: imagesSend.url})
+    
     }
-  };
-  
+    
+    else if (!say) {
+      await msg.channel.send("Example: <prefix>.say test");
+      return msg.delete(); // Menghapus pesan pengguna jika tidak ada teks
+    }
+      
+      
+
+    if (say.length > 250) {
+      await msg.channel.send("The maximum character was 250")
+      return msg.delete(); // Menghapus pesan pengguna jika teks melebihi 250 karakter
+    }
+
+    say = say.replace("@everyone", "@everyone").replace("@here", "@here").replace("ilham", "lu kontol "+msg.member.user.username)
+
+    await msg.channel.send(say);
+    msg.delete(); // Menghapus pesan pengguna setelah bot mengirimkan pesan
+  }
+};
