0869d6bd4cf4b1a7cbecde40904db0f9741bd5a3
b1theaven <mohammadrizky881@gmail.com>
2024-08-19 09:45:41 +0700

    update command list on help

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,7 @@ module.exports = {
                 { name: '<:shield:885185036419219496> Moderation', value: stripIndents`
 > \`setprefix\`, \`setlanguage\`, \`clear\`, \`addemoji\`, \`backup\`, \`kick\`, \`ban\`, \`giveaway\`, \`nuke\`, \`unban\`, \`reminder\`, \`removeremind\`` },
                 { name: "<:chart:885185036419219496> Slash "+ lang(msg.guild, "COMMANDS"), value: stripIndents`
-> \`avatar\`, \`imagine\`, \`serveravatar\`, \`automod-flaggedword\`, \`automod-keyword\`, \`automod-mentionspam\`, \`ban\`, \`kick\`, \`setlanguage\`, \`setprefix\`, \`leaderboard\`, \`reloadcommands\`, \`lyrics\`, \`youtubedownload\`, \`reactionrole-add\`, \`reactionrole-remove\`` },
+> \`avatar\`, \`imagine\`, \`confess\`, \`bmkg\`, \`translate\`, \`confession-blacklist\`, \`confession-remove\`, \`confession-setup\`, \`confession-unblacklist\`, \`serveravatar\`, \`playstore\`, \`appstore\`, \`automod-flaggedword\`, \`automod-keyword\`, \`automod-mentionspam\`, \`ban\`, \`kick\`, \`setlanguage\`, \`setprefix\`, \`leaderboard\`, \`reloadcommands\`, \`lyrics\`, \`youtubedownload\`, \`reactionrole-add\`, \`reactionrole-remove\`` },
                 { name: '<:information_source:885192830283313232> Information ' + lang(msg.guild, 'COMMANDS'), value: stripIndents`
 > \`steam\`, \`playstore\`, \`topgginfo\`, \`device\`, \`worldclock\`, \`permissions\`, \`whois\`, \`stats\`, \`leaderboard\`` },
             )
