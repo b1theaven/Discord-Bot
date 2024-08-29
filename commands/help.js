566c23ba3cffceae5c58a7b18cd8a71c6b2d1359
b1theaven <mohammadrizky881@gmail.com>
2024-08-29 10:42:55 +0700

    update list command

--- commands/help.js
+++ commands/help.js
@@ -14,13 +14,13 @@ module.exports = {
                 { name: '<:crystal_ball:885185036419219496> Boosters', value: stripIndents`
 > \`role create\`, \`role edit\`, \`role give\`` },
                 { name: '<:hammer:885195095840804885> Utility ' + lang(msg.guild, 'COMMANDS'), value: stripIndents`
-> \`avatar\`, \`ping\`, \`youtubedownload\`, \`emojilist\`, \`movie\`, \`say\`, \`remindme\`, \`cancelremind\`, \`listreminder\`, \`snipe\`, \`google\`, \`ytstats\`, \`afk\`, \`nasa\`, \`cuddle\`, \`hug\`, \`kiss\`, \`pat\`, \`slap\`, \`youtube\`` },
-                { name: '<:shield:885185036419219496> Moderation', value: stripIndents`
-> \`setprefix\`, \`setlanguage\`, \`clear\`, \`addemoji\`, \`backup\`, \`kick\`, \`ban\`, \`giveaway\`, \`nuke\`, \`unban\`, \`reminder\`, \`removeremind\`` },
+> \`avatar\`, \`ping\`, \`youtubedownload\`, \`emojilist\`, \`downloader\`, \`help\`, \`pinterest\`, \`movie\`, \`say\`, \`remindme\`, \`cancelremind\`, \`listreminder\`, \`snipe\`, \`google\`, \`ytstats\`, \`afk\`, \`nasa\`, \`cuddle\`, \`hug\`, \`kiss\`, \`pat\`, \`slap\`, \`youtube\`` },
+                { name: '<:shield:885185036419219496> Moderation Commands & Slash Commands', value: stripIndents`
+> \`setprefix\`, \`setlanguage\`, \`reloadcommands\`, \`blacklist\`, \`reactionrole-add\`, \`reactionrole-remove\`, \`automod-flaggedword\`, \`automod-keyword\`, \`automod-mentionspam\`, \`ban\`, \`kick\`, \`setlanguage\`, \`setprefix\`, \`clear\`, \`addemoji\`, \`confession-delete\`, \`confession-blacklist\`, \`confession-remove\`, \`confession-setup\`, \`confession-unblacklist\`, \`eval\`, \`backup\`, \`kick\`, \`ban\`, \`giveaway\`, \`nuke\`, \`unban\`, \`reminder\`, \`removeremind\`` },
                 { name: "<:chart:885185036419219496> Slash "+ lang(msg.guild, "COMMANDS"), value: stripIndents`
-> \`avatar\`, \`imagine\`, \`confess\`, \`eval\`, \`bmkg\`, \`translate\`, \`confession-blacklist\`, \`confession-remove\`, \`confession-setup\`, \`confession-unblacklist\`, \`serveravatar\`, \`playstore\`, \`appstore\`, \`automod-flaggedword\`, \`automod-keyword\`, \`automod-mentionspam\`, \`ban\`, \`kick\`, \`setlanguage\`, \`setprefix\`, \`leaderboard\`, \`reloadcommands\`, \`lyrics\`, \`youtubedownload\`, \`reactionrole-add\`, \`reactionrole-remove\`` },
+> \`avatar\`, \`imagine\`, \`confess\`, \`bmkg\`, \`translate\`, \`serveravatar\`, \`playstore\`, \`appstore\`, \`leaderboard\`, \`lyrics\`, \`movie\`, \`pinterest\`, \`youtubedownload\`` },
                 { name: '<:information_source:885192830283313232> Information ' + lang(msg.guild, 'COMMANDS'), value: stripIndents`
-> \`steam\`, \`playstore\`, \`topgginfo\`, \`device\`, \`worldclock\`, \`permissions\`, \`whois\`, \`stats\`, \`leaderboard\`` },
+> \`steam\`, \`playstore\`, \`topgginfo\`, \`device\`, \`appstore\`, \`firstmessage\`, \`worldclock\`, \`permissions\`, \`whois\`, \`stats\`, \`leaderboard\`` },
             )
             .setFooter('Apabila terdapat bug pada bot ini silahkan gunakan a.reportbug [bug] , terimakasih!');
             
