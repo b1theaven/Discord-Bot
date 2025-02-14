444539a4bfb39d3d9a99666ab5f25382f1eed558
b1theaven <mohammadrizky881@gmail.com>
2025-02-14 10:29:21 +0700

    refactor the code and add filter to use this command

--- commands/say.js
+++ commands/say.js
@@ -2,30 +2,48 @@ module.exports = {
   name: "say",
   run: async (client, msg, args) => {
     let say = args.join(" ");
-    const imagesSend = msg.attachments.first()
-    console.log(imagesSend)
-    
-    if(imagesSend) {
-       await msg.delete()
-       return msg.channel.send({ content: imagesSend.url})
-    
-    }
-    
-    else if (!say) {
+    const imagesSend = msg.attachments.first();
+    console.log(imagesSend);
+
+    // Role yang diizinkan
+    const allowedRoles = [
+      "ADMIN_ID",
+      "ADMIN_ID",
+      "ADMIN_ID",
+      "ADMIN_ID",
+      "ADMIN_ID",
+    ];
+
+    if (imagesSend) {
+      await msg.delete();
+      return msg.channel.send({ content: imagesSend.url });
+    } else if (!say) {
       await msg.channel.send("Example: <prefix>.say test");
-      return msg.delete(); // Menghapus pesan pengguna jika tidak ada teks
+      return msg.delete();
     }
-      
-      
 
     if (say.length > 250) {
-      await msg.channel.send("The maximum character was 250")
-      return msg.delete(); // Menghapus pesan pengguna jika teks melebihi 250 karakter
+      await msg.channel.send("The maximum character was 250");
+      return msg.delete();
     }
 
-    say = say.replace("@everyone", "@everyone").replace("@here", "@here").replace("ilham", "lu kontol "+msg.member.user.username)
+    // Cek apakah user memiliki role yang diizinkan
+    const hasAllowedRole = msg.member.roles.cache.some((role) =>
+      allowedRoles.includes(role.id)
+    );
+
+    // Jika user tidak memiliki role yang diizinkan dan mencoba mention @everyone atau @here, kirim pesan ephemeral
+    if (
+      !hasAllowedRole &&
+      (say.includes("@everyone") || say.includes("@here"))
+    ) {
+      return msg.reply({
+        content:
+          "❌ Anda tidak dapat menggunakan `@everyone` atau `@here` dalam perintah ini!",
+      });
+    }
 
     await msg.channel.send(say);
-    msg.delete(); // Menghapus pesan pengguna setelah bot mengirimkan pesan
-  }
+    msg.delete();
+  },
 };
